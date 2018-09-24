d2cb0c69d468997c8554a298d9adc1e03b6a0318
Alexander Tokos <alext_1995@hotmail.com>
2018-09-24 15:20:36 +0300

    created model for aution

--- auctions/models.py
+++ auctions/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
 
-# Create your models here.
+class Auction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    deadline = models.DateTimeField(default=datetime.datetime.now()+datetime.timedelta(hours=72))
--- auctions/views.py
+++ auctions/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
+def list_view(request):
     return HttpResponse("This is the index page")
