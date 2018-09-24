aba0d3469386f18e35c5699b4a6c83ea87d16edf
Alexander Tokos <alext_1995@hotmail.com>
2018-09-24 16:35:47 +0300

    added link to detail in title of the auction

--- auctions/urls.py
+++ auctions/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "auctions"
 
 urlpatterns = [
-    path('', views.list_view, name = 'list'),
-    path('<int:auction_id>/', views.details_view, name = 'detail'),
+    path('', views.list, name = 'list'),
+    path('<int:auction_id>/', views.detail, name = 'detail'),
 ]
--- auctions/views.py
+++ auctions/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 from auctions.models import Auction
 
 # Create your views here.
-def list_view(request):
+def list(request):
     auctions_list = Auction.objects.all()
     context = {'auctions_list': auctions_list}
     return render(request, 'auctions/list.html', context)
 
-def details_view(request):
+def detail(request):
     HttpResponse("details")
