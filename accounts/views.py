541cc521fec3a078fc3ada9533cc021fdf5b0aed
atokos <alext_1995@hotmail.com>
2018-09-30 21:17:05 +0300

    url path and view for signup added

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 
-def profile(request):
+def profile_view(request):
     return render(request, 'accounts/profile.html')
+
+
+def signup_view(request):
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # TODO log user in
+            return redirect('homepage')
+    else:
+        form = UserCreationForm()
+        context = {'form': form}
+    return render(request, 'accounts/signup.html', context)
