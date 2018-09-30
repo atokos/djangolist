7fe4833ce7416b3b2089a255e2f724235d3d869e
Alexander Tokos <alext_1995@hotmail.com>
2018-09-30 20:45:08 +0300

    added urls namespace for auctions app

--- auctions/urls.py
+++ auctions/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'auctions'
+
 urlpatterns = [
     path('', views.auction_list, name='list'),
     path('<int:auction_id>/', views.auction_detail, name='detail'),
