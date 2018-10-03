18bf94d26513dbd4a10304415d1aa8da64c8054c
atokos <alext_1995@hotmail.com>
2018-10-03 19:58:01 +0300

    added backend for emails, simplified the homepage display using TemplateView

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout
 
 
 def signup_view(request):
-
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+
             login(request, user)
             return redirect('homepage')
     else:
@@ -38,7 +39,7 @@ def logout_view(request):
         logout(request)
         return redirect('homepage')
 
-
+@login_required()
 def profile_view(request):
     return render(request, 'accounts/profile.html')
 
--- djangolist/urls.py
+++ djangolist/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
-from . import views
+from django.views.generic.base import TemplateView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='homepage.html'), name="homepage"),
     path('admin/', admin.site.urls),
     path('auctions/', include('auctions.urls')),
     path('accounts/', include('accounts.urls')),
-    path('', views.homepage, name="homepage"),
+
 ]
 
 urlpatterns += staticfiles_urlpatterns()
